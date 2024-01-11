c52e789e20ecdb95d54c8ef2ab90c393c59cfde5
xavierporre <xavierdebau@gmail.com>
2024-01-11 10:23:03 -0300

    estructura de auth basica

---
+++ Bau_app/src/components/Authenticate.svelte
@@ -0,0 +1,65 @@
+<script>
+    let user_name = "";
+    let password = "";
+    let error = false;
+
+    function handleAuth(){
+        if (!user_name || !password){
+            error = true;
+            return;
+        }
+    }
+
+</script>
+
+
+
+<div class="AuthContainer">
+    <form>
+        <h1>Login</h1>
+        {#if error}
+            <p class="error">Credenciales ingresadas son incorrectas</p>
+        {/if}
+        <label>
+             <input bind:value={user_name} type="user_name" placeholder="nombre"/> 
+        </label>
+        <label>
+            <input bind:value={password} type="password" placeholder="contraseña"/>
+        </label>
+        <button type="submit" >ingresar</button>
+    </form>
+</div>   
+
+<style>
+    .AuthContainer{
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        justify-content: center;
+        
+    }
+    form {
+        display: flex;
+        flex-direction: column;
+        gap: 8px;
+        width: 400px;
+        max-width: 100%;
+        margin: 0 auto;
+    }
+    form input{
+        width: 100%;
+    }
+    form input:focus {
+        border: none;
+        outline: 1px solid #013200
+    }
+    form button{
+        background: whitesmoke;
+        color: black
+    }
+    form button:hover {
+        background: navy;
+        color: white
+    }
+    .error { color: coral;}
+</style>
---
+++ Bau_app/src/routes/+layout.svelte
@@ -0,0 +1,16 @@
+
+
+<dib class="mainContainer">
+    <slot/>
+</dib>
+
+<style>
+    .mainContainer {
+        min-height: 100vh;
+        background: linear-gradient(to bottom,#000428,#0a7386);
+        color: white;
+        position: relative;
+        display: flex;
+        flex-direction: column;
+    }
+</style>
