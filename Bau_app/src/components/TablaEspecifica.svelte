4a5702390493edf2176e5aa9b1217270ddcbffe2
xavierporre <xavierdebau@gmail.com>
2024-03-01 13:10:31 -0300

    opcion de borrar

--- Bau_app/src/components/TablaEspecifica.svelte
+++ Bau_app/src/components/TablaEspecifica.svelte
@@ -3,7 +3,7 @@
     import { onMount } from 'svelte';
     import { writable } from 'svelte/store';  
     import { tablaGeneral, auth, tablaEspecifica } from '../lib/firebase/firebase';
-    import { addDoc, onSnapshot } from 'firebase/firestore';
+    import { addDoc, onSnapshot, doc, deleteDoc} from 'firebase/firestore';
 
 // Definir las variables estaticas para respuesta de formulario
     const componentes = [
@@ -62,6 +62,7 @@
         nombreComponente = '';
     };
 
+
     // Función para manejar el envío del formulario
     async function handleSubmit() {
         const id_creador = getUserId();
@@ -91,6 +92,16 @@
             console.error('Error al agregar los datos del activo a Firestore:', error);
         }
     };
+    // Función para borrar un elemnto
+    async function handleDelete(id: string) {
+        try {
+            await deleteDoc(doc(tablaEspecifica, id));
+            console.log('Activo eliminado con éxito.');
+        } catch (error) {
+            console.error('Error al eliminar el activo:', error);
+        }
+    }
+
      
     // Función para cargar los activos anteriores desde Firestore y actualizar el store
     async function cargarActivosAnteriores() {
@@ -235,6 +246,14 @@
             <td>{activo.norma}</td>
             <td>{activo.estadoNorma}</td>
             <td>{activo.comentarios}</td>
+
+            <td>
+                <button class="bg-red-500 text-white px-2 py-1 rounded-md transition duration-300 ease-in-out hover:bg-red-600" on:click={() => handleDelete(activo.id)}>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                        <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+                    </svg>
+                </button>
+            </td>
             
         </tr>
         {/each}
